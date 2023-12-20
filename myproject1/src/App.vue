f0669b10596a0c65d86321ffdbcf3c7f84f53588
Uladlena10 <vladlena.igorevna@gmail.com>
2023-12-20 22:07:03 +0300

    Добавлены файлы проекта

---
+++ myproject1/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <HeaderComponent></HeaderComponent>
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view></router-view>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "./components/layout/HeaderComponent";
+// import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    HeaderComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
